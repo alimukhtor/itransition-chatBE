a990736627fa8dedeace25347410fe5dab4e5dc0
Izzatillo <izzatillo.mukhtorov@ydistri.com>
2022-09-27 15:56:48 +0500

    fix cors issues

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ const io = new Server(httpServer,  {
     transports:['polling'],
     cors:{
       cors: {
-        origin: "https://itransition-chat.vercel.app/"
+        origin: "http://localhost:3000"
       }
     }});
 
